7ecdc7764ccc1a90ceabe330e355311c74fa9eb7
Malik Ivory <300kleek@gmail.com>
2026-09-01 17:28:47 -0400

    fix(oidc): use StringLike for repo subject match in trust policy

--- terraform/github_oidc.tf
+++ terraform/github_oidc.tf
@@ -32,7 +32,9 @@ resource "aws_iam_role" "github_actions_oidc_role" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:IvoryCloudOps/regional-distribution-operations-platform:ref:refs/heads/main"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:IvoryCloudOps/regional-distribution-operations-platform:*"
           }
         }
       }
